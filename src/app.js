7a93cb140adb4a32d307e3ecf68f8d5d96b12540
Elvis Duvergé <e.duverge@nexus.com.do>
2020-09-10 12:22:59 -0400

    add cors package

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose.js")
 const userRouter = require("./routers/user.js")
 
 const app = express();
-
 const port = process.env.PORT;
 
 app.use(express.json())
+app.use(cors());
 app.use(userRouter);
 
 app.listen(port, () => {
