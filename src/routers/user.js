3b93a12f57893cae28cc2e34b1d835ecc04bcace
Elvis Duvergé <e.duverge@nexus.com.do>
2020-09-10 12:35:29 -0400

    add fix login router

--- src/routers/user.js
+++ src/routers/user.js
@@ -21,7 +21,7 @@ router.post("/user/create", verifySignUp, async (req, res) => {
 //Log In User
 router.post("/user/login", async (req, res) => {
     try{
-        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const user = await User.findByCredentials(req.body.username, req.body.password);
         const token = await user.generateAuthToken();
         
         res.send({user, token});
